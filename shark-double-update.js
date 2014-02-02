afceff76ac971d2f00d7ad72f32e6646f5211d41
Vaughn Iverson <vsi@Vaughns-Mac-mini-2.local>
2014-02-01 17:26:10 -0800

    Updated setInterval example to illustrate the difference between handlers returning objects and those returning other types (array, string, number)

--- shark-double-update.js
+++ shark-double-update.js
@@ -3,16 +3,47 @@ if (Meteor.isClient) {
   if (typeof console !== 'undefined')
     console.log("Meteor version: " + Meteor.release);
 
-  var updates = 0;
-  Session.set('clicks',0);
+  var numberUpdates = 0;
+  var arrayUpdates = 0;
+  var stringUpdates = 0;
+  var objectStringUpdates = 0;
+  var objectNumberUpdates = 0;
+  var objectArrayUpdates = 0;
+
+  Session.set('ticks',0);
   Template.hello.greeting = function () {
     return "Welcome to shark-double-update.";
   };
 
-  Template.hello.clicks = function () {
-    updates++;
-    return [Session.get('clicks'), updates];
+  Template.hello.numberTicks = function () {
+    numberUpdates++;
+    return Session.get('ticks') + numberUpdates/100000;
   }
 
-  Meteor.setInterval((function () {Session.set('clicks',Session.get('clicks')+1)}), 1000);
+  Template.hello.arrayTicks = function () {
+    arrayUpdates++;
+    return [Session.get('ticks'), arrayUpdates];
+  }
+
+  Template.hello.stringTicks = function () {
+    stringUpdates++;
+    return JSON.stringify([Session.get('ticks'), stringUpdates]);
+  }
+
+  Template.hello.objectStringTicks = function () {
+    objectStringUpdates++;
+    return { counts: JSON.stringify([Session.get('ticks'), objectStringUpdates]) };
+  }
+
+  Template.hello.objectArrayTicks = function () {
+    objectArrayUpdates++;
+    return { counts: [Session.get('ticks'), objectArrayUpdates] };
+  }
+
+  Template.hello.objectNumberTicks = function () {
+    objectNumberUpdates++;
+    return { tickCounts: Session.get('ticks'), updateCounts: objectNumberUpdates };
+  }
+
+  Meteor.setInterval((function () {Session.set('ticks', Session.get('ticks')+1)}), 1000);
 }
